22d8d98e472293edfe16d4e56f07110d7a91cb8c
TAeng <T4eng.trirong@gmail.com>
2018-05-19 04:28:53 +0700

    [+] use api service

--- test-b/src/app/app.component.ts
+++ test-b/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http'
+import { environment } from '../environments/environment'
+import { ApiService } from './service/api.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  constructor(
+    private http:HttpClient,
+    private apiService: ApiService
+  ){}
+
+  showComponent = false
+
+ ngOnInit(){
+
+      this.apiService.getApi().toPromise().then( res=>{ 
+         localStorage.setItem('persons',JSON.stringify(res))
+         this.showComponent = true
+      })
+  
+  }
 }
