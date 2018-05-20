e1f96ce814b92d55d04cd770a6239cb52cb84ec7
TAeng <T4eng.trirong@gmail.com>
2018-05-20 19:51:48 +0700

    [modify] format code

--- test-b/src/app/component/day.component.ts
+++ test-b/src/app/component/day.component.ts
@@ -20,20 +20,13 @@ export class DayComponent implements OnInit{
 
   constructor(
     private http:HttpClient
-   
   ){ }
 
-  dataName=[];
-  dataPhoto=[];
+  dataName=[]; dataPhoto=[];
+  arrySun=[];arryMon=[];arryTue=[];
+  arryWed=[];arryThu=[];arryFri=[];arrySat=[];
+  noPerson;
 
-  arrySun=[];
-  arryMon=[];
-  arryTue=[];
-  arryWed=[];
-  arryThu=[];
-  arryFri=[];
-  arrySat=[];
-  Noday;
   ngOnInit(){
   
 
@@ -50,11 +43,6 @@ export class DayComponent implements OnInit{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
               }
-                var del = this.countMax-this.countSun
-                if(del){
-                this.arrySun = Array(del).fill(4)
-                }
-              
             }
             if(myDate.getDay()==1&&this.toDay.toUpperCase()=='MON'){
               if(element.name==""){  
@@ -64,11 +52,6 @@ export class DayComponent implements OnInit{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
               }
-                var del = this.countMax-this.countMon
-                if(del){
-                this.arryMon = Array(del).fill(4)
-                }
-              
             }
             if(myDate.getDay()==2&&this.toDay.toUpperCase()=='TUE'){
               if(element.name==""){  
@@ -78,11 +61,6 @@ export class DayComponent implements OnInit{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
               }
-                var del = this.countMax-this.countTue
-                if(del){
-                this.arryTue = Array(del).fill(4)
-                }
-              
             }
             if(myDate.getDay()==3&&this.toDay.toUpperCase()=='WED'){
               if(element.name==""){  
@@ -91,12 +69,7 @@ export class DayComponent implements OnInit{
               }else{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
-              }
-                var del = this.countMax-this.countWed
-                if(del){
-                this.arryWed = Array(del).fill(4)
-                }
-                
+              } 
             }
             if(myDate.getDay()==4&&this.toDay.toUpperCase()=='THU'){
               if(element.name==""){  
@@ -106,11 +79,6 @@ export class DayComponent implements OnInit{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
               }
-                var del = this.countMax-this.countThu
-                if(del){
-                this.arryThu = Array(del).fill(4)
-                }
-               
             }
             if(myDate.getDay()==5&&this.toDay.toUpperCase()=='FRI'){
               if(element.name==""){  
@@ -120,11 +88,6 @@ export class DayComponent implements OnInit{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
               }
-                var del = this.countMax-this.countFri
-                if(del){
-                this.arryFri = Array(del).fill(4)
-                }
-               
             }
             if(myDate.getDay()==6&&this.toDay.toUpperCase()=='SAT'){
               if(element.name==""){  
@@ -134,15 +97,51 @@ export class DayComponent implements OnInit{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
               }
-                var del = this.countMax-this.countSat
-                if(del){
-                this.arrySat = Array(del).fill(4)
-                }
-               
             }
           })
         }
-     
+
+      if(this.countSun!=undefined){
+         var del = this.countMax-this.countSun
+         if(del){ this.arrySun = Array(del).fill(4) }
+         if(del == this.countMax) { this.noPerson = "No one today" }
       }
-  }
+
+      if(this.countMon!=undefined){
+        var del = this.countMax-this.countMon
+        if(del){ this.arryMon = Array(del).fill(4) }
+        if(del == this.countMax) { this.noPerson = "No one today" }
+     }
+
+      if(this.countTue!=undefined){
+        var del = this.countMax-this.countTue
+        if(del){ this.arryTue = Array(del).fill(4) }
+        if(del == this.countMax) { this.noPerson = "No one today" }
+      }
+
+      if(this.countWed!=undefined){
+         var del = this.countMax-this.countWed
+         if(del){ this.arryWed = Array(del).fill(4) }
+         if(del == this.countMax) { this.noPerson = "No one today" }
+      }
+
+      if(this.countThu!=undefined){
+        var del = this.countMax-this.countThu
+        if(del){ this.arryThu = Array(del).fill(4) }
+        if(del == this.countMax) { this.noPerson = "No one today" }
+     }
+
+     if(this.countFri!=undefined){
+       var del = this.countMax-this.countFri
+       if(del){ this.arryFri = Array(del).fill(4) }
+       if(del == this.countMax) { this.noPerson = "No one today" }
+   }
+
+    if(this.countSat!=undefined){
+      var del = this.countMax-this.countSat
+      if(del){ this.arrySat = Array(del).fill(4) }
+      if(del == this.countMax) { this.noPerson = "No one today" }
+   }
+ }
+}
 
