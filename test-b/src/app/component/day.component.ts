9f513acfa9085c0eb33066a33605322f8bf1a8e7
TAeng <T4eng.trirong@gmail.com>
2018-05-20 17:08:43 +0700

    [+] space in day

--- test-b/src/app/component/day.component.ts
+++ test-b/src/app/component/day.component.ts
@@ -9,51 +9,140 @@ import { environment } from '../../environments/environment'
 })
 export class DayComponent implements OnInit{
     @Input() toDay: string
+    @Input() countMax: number
+    @Input() countSun: number
+    @Input() countMon: number
+    @Input() countTue: number
+    @Input() countWed: number
+    @Input() countThu: number
+    @Input() countFri: number
+    @Input() countSat: number
 
   constructor(
     private http:HttpClient
-  ){}
+   
+  ){ }
 
   dataName=[];
   dataPhoto=[];
 
+  arrySun=[];
+  arryMon=[];
+  arryTue=[];
+  arryWed=[];
+  arryThu=[];
+  arryFri=[];
+  arrySat=[];
+  Noday;
   ngOnInit(){
- 
+  
+
         var per = JSON.parse(localStorage.getItem('persons'))
         if(localStorage.getItem('persons')){
           var myDate;
           per.forEach(element => {
             myDate = new Date (element.birthday.mdy)
             if(myDate.getDay()==0&&this.toDay.toUpperCase()=='SUN'){
+              if(element.name==""){  
+              this.dataName.push("noname")
+              this.dataPhoto.push(element.photo)
+              }else{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
+              }
+                var del = this.countMax-this.countSun
+                if(del){
+                this.arrySun = Array(del).fill(4)
+                }
+              
             }
             if(myDate.getDay()==1&&this.toDay.toUpperCase()=='MON'){
+              if(element.name==""){  
+                this.dataName.push("noname") 
+                this.dataPhoto.push(element.photo)
+              }else{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
+              }
+                var del = this.countMax-this.countMon
+                if(del){
+                this.arryMon = Array(del).fill(4)
+                }
+              
             }
             if(myDate.getDay()==2&&this.toDay.toUpperCase()=='TUE'){
+              if(element.name==""){  
+                this.dataName.push("noname") 
+                this.dataPhoto.push(element.photo)
+              }else{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
+              }
+                var del = this.countMax-this.countTue
+                if(del){
+                this.arryTue = Array(del).fill(4)
+                }
+              
             }
             if(myDate.getDay()==3&&this.toDay.toUpperCase()=='WED'){
+              if(element.name==""){  
+                this.dataName.push("noname")
+                this.dataPhoto.push(element.photo)
+              }else{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
+              }
+                var del = this.countMax-this.countWed
+                if(del){
+                this.arryWed = Array(del).fill(4)
+                }
+                
             }
             if(myDate.getDay()==4&&this.toDay.toUpperCase()=='THU'){
+              if(element.name==""){  
+                this.dataName.push("noname")
+                this.dataPhoto.push(element.photo)
+              }else{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
+              }
+                var del = this.countMax-this.countThu
+                if(del){
+                this.arryThu = Array(del).fill(4)
+                }
+               
             }
             if(myDate.getDay()==5&&this.toDay.toUpperCase()=='FRI'){
+              if(element.name==""){  
+                this.dataName.push("noname") 
+                this.dataPhoto.push(element.photo)
+              }else{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
+              }
+                var del = this.countMax-this.countFri
+                if(del){
+                this.arryFri = Array(del).fill(4)
+                }
+               
             }
             if(myDate.getDay()==6&&this.toDay.toUpperCase()=='SAT'){
+              if(element.name==""){  
+                this.dataName.push("noname") 
+                this.dataPhoto.push(element.photo)
+              }else{
               this.dataName.push(element.name)
               this.dataPhoto.push(element.photo)
+              }
+                var del = this.countMax-this.countSat
+                if(del){
+                this.arrySat = Array(del).fill(4)
+                }
+               
             }
           })
         }
+     
       }
   }
 
